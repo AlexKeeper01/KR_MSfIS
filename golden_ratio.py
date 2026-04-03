a6c3949042cde98b4f5cbf706a01015db7253c9d
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2026-04-03 10:03:49 +0300

    Create golden_ratio.py

---
+++ golden_ratio.py
@@ -0,0 +1,67 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return math.log(x) + 2 * math.sin(x)
+
+def golden_section_search(a, b, eps):
+    phi = (math.sqrt(5) - 1) / 2
+
+    x1 = b - phi * (b - a)
+    x2 = a + phi * (b - a)
+
+    f1 = f(x1)
+    f2 = f(x2)
+
+    while abs(b - a) > eps:
+        if f1 > f2:
+            a = x1
+            x1 = x2
+            f1 = f2
+            x2 = a + phi * (b - a)
+            f2 = f(x2)
+        else:
+            b = x2
+            x2 = x1
+            f2 = f1
+            x1 = b - phi * (b - a)
+            f1 = f(x1)
+
+    xmin = (a + b) / 2
+    return xmin, f(xmin)
+
+
+a = 2
+b = 6
+eps = 0.02
+xmin, fmin = golden_section_search(a, b, eps)
+
+print(f"x_min ≈ {xmin:.4f}")
+print(f"f(x_min) ≈ {fmin:.4f}")
+
+x_vals = np.linspace(0.01, 10, 500)
+y_vals = [f(x) for x in x_vals]
+
+plt.figure(figsize=(10, 6))
+plt.plot(x_vals, y_vals, label="f (x)")
+ax = plt.gca()
+ax.spines['left'].set_position('zero')
+ax.spines['bottom'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+
+plt.scatter(xmin, fmin)
+
+plt.axvline(x=xmin, linestyle="--")
+plt.axhline(y=fmin, linestyle="--")
+plt.title("Метод золотого сечения")
+plt.xlabel("x")
+plt.ylabel("f (x)")
+plt.legend()
+plt.grid()
+
+plt.show()
