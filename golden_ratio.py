7bfaf51282537687558c71f00066977affaadea2
p1v0zavr1k <60043091+AlexKeeper01@users.noreply.github.com>
2026-04-03 10:06:03 +0300

    Update golden_ratio.py

--- golden_ratio.py
+++ golden_ratio.py
@@ -14,8 +14,14 @@ def golden_section_search(a, b, eps):
 
     f1 = f(x1)
     f2 = f(x2)
+    iteration = 1
 
     while abs(b - a) > eps:
+        print(f"Итерация {iteration}:")
+        print(f"a = {a:.4f}, b = {b:.4f}")
+        print(f"x1 = {x1:.4f}, f(x1) = {f1:.4f}")
+        print(f"x2 = {x2:.4f}, f(x2) = {f2:.4f}")
+        print("-" * 40)
         if f1 > f2:
             a = x1
             x1 = x2
@@ -29,6 +35,8 @@ def golden_section_search(a, b, eps):
             x1 = b - phi * (b - a)
             f1 = f(x1)
 
+        iteration += 1
+
     xmin = (a + b) / 2
     return xmin, f(xmin)
 
@@ -38,7 +46,7 @@ b = 6
 eps = 0.02
 xmin, fmin = golden_section_search(a, b, eps)
 
-print(f"x_min ≈ {xmin:.4f}")
+print(f"\nx_min ≈ {xmin:.4f}")
 print(f"f(x_min) ≈ {fmin:.4f}")
 
 x_vals = np.linspace(0.01, 10, 500)
